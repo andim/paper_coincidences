c1a1f7a7a5de6da8ec9eb6185c8a529d92959621
Andreas Mayer <andisspam@gmail.com>
2022-12-09 22:21:47 +0000

    add metadata file

---
+++ data/README.md
@@ -0,0 +1,3 @@
+Most data is automatically downloaded from publically accesible databases, as specified in the Snakemake workflow.
+
+The file 'metadata_tanno.csv' is extracted from Table S1 of the original study (Tanno et al. PNAS 2020). Preprocessed sequencing data was courteously provided by the study authors, and can be obtained from them by request.
---
+++ lib.py
@@ -0,0 +1,13 @@
+data_directory = 'data/'
+
+tannofiles = ['D1', 'D2', 'E1', 'E2', 'F1', 'F2',
+              'X', 'Y', 'Z',
+              'A1 memory', 'A1 naive',
+              'A2 memory', 'A2 naive',
+              'B1 memory', 'B1 naive',
+              'B2 memory', 'B2 naive',
+              'C1 memory', 'C1 naive',
+              'C2 memory', 'C2 naive',
+              ]
+
+
